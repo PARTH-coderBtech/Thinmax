bd57766f321fabac20919576feb9a1835a1bb2d8
PARTH-coderbtech <parth63970@gmail.com>
2025-06-16 19:05:09 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -24,6 +24,8 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
 }).then(() => {
   console.log("MongoDB connected");
-  app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 }).catch(err => console.error("MongoDB connection error:", err));
 
--- server/testenv.js
+++ server/testenv.js
@@ -2,3 +2,5 @@ const dotenv = require('dotenv');
 dotenv.config();
 console.log('Razorpay Key ID:', process.env.RAZORPAY_KEY);
 console.log('Razorpay Key Secret:', process.env.RAZORPAY_SECRET);
+console.log("Loaded MONGO_URI:", process.env.MONGO_URI);
+
